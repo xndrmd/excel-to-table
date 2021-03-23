e48a77ec7bacd16dde992d7db433e65d8775e515
Macedo, Alexander (TSMLP) <Alexander.Macedo@kantar.com>
2021-03-22 22:03:37 -0500

    add width properties

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ input.addEventListener('change', () => {
 
     readXlsxFile(file).then((rows) => {
         const htmlrows = rows.map(row => `<tr>${row.map(col => `<td>${col}</td>`).join('')}</tr>`).join('');
-        const htmltable = `<table class="table table-bordered table-striped">${htmlrows}</table>`;
+        const htmlcolgroups = `<colgroup><col style="width: 230px;" span="1"></colgroup>`
+        const htmltable = `<table class="table table-bordered table-striped" style="width: 100%;">${htmlcolgroups}${htmlrows}</table>`;
         result.value = htmltable;
     });
 });
